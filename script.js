41be637ca699992a42bac0582510d932eda21f92
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2024-12-29 17:41:52 +0800

    feat: add operate to handle all operation

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(firstNum, secondNum, operation) {
+  switch(operation) {
+    case '+':
+      console.log(add(firstNum, secondNum));
+      break;
+    case '-':
+      console.log(subtract(firstNum, secondNum));
+      break;
+    case '*':
+      console.log(multiply(firstNum, secondNum));
+      break;
+    case '/':
+      console.log(divide(firstNum, secondNum));
+      break;
+  }
+}
+
+let firstNum;
+let secondNum;
+let operation;
